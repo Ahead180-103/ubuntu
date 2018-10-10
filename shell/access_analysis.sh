5dd11ac3770f9e00ea076972a1fb57599ec937d0
gaoyong <ahead_888@163.com>
2018-10-10 13:50:49 +0800

    add

--- shell/access_analysis.sh
+++ shell/access_analysis.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 year=`date +%Y`
 echo $year
+year_ago=`date -d "1 year ago" +%Y`
+echo ${year_ago}
 
-month=`date`
-month=`echo $month | awk '{print $2}'`
+month=`date +%h`
 echo $month
+month_ago=`date -d "1 month ago"  +%b`
+echo ${month_ago}
 
 day=`date +%d`
 echo $day
+day_ago=`date -d "1 day ago" +%d`
+echo ${day_ago}
+
+sed -i  "/^awk/s@\b${day_ago}\b@${day}@g"  /root/access_analysis.sh  &>/dev/null
+sed -i  "/^awk/s@\b${month_ago}\b@${month}@g"  /root/access_analysis.sh  &>/dev/null
+sed -i  "/^awk/s@\b${year_ago}\b@${year}@g"  /root/access_analysis.sh    &>/dev/null
+
 
 datetime=`date`
-echo $datetime >> /opt/access_analysis/access.txt
+echo $datetime >> /opt/access_analysis/access`date +%y%m%d-%H%M`.txt
 
-awk '/09\/Oct\/2018/{x[$1]++}END{for(i in x)if(x[i]>100){print x[i],i}}' /var/log/nginx/access.log | sort -nr >> /opt/access_analysis/access.txt
+awk '/10\/Oct\/2018/{x[$1]++}END{for(i in x)if(x[i]>0){print x[i],i}}' /var/log/nginx/access.log | sort -nr >> /opt/access_analysis/access`date +%y%m%d-%H%M`.txt
 
-awk '/09\/Oct\/2018/{x[$1]++}END{for(i in x)if(x[i]>100){print x[i],i}}' /var/log/nginx/access.log.1 | sort -nr >> /opt/access_analysis/access.txt
-echo "" >> /opt/access_analysis/access.txt
-echo ""  >> /opt/access_analysis/access.txt
+awk '/10\/Oct\/2018/{x[$1]++}END{for(i in x)if(x[i]>0){print x[i],i}}' /var/log/nginx/access.log.1 | sort -nr >> /opt/access_analysis/access`date +%y%m%d-%H%M`.txt
+echo "" >> /opt/access_analysis/access`date +%y%m%d-%H%M`.txt
+echo ""  >> /opt/access_analysis/access`date +%y%m%d-%H%M`.txt

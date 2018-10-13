051213faea527704966bded021b8ab6a2fa97bed
gaoyong <ahead_888@163.com>
2018-10-13 17:47:11 +0800

    add

---
+++ python/shell.py/print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#此实例为标准输出print的用法
+print(1,2,3,4)   #    1 2 3 4 
+print("以下是给定sep='#'的打印方式")
+print(1,2,3,4,sep="###")  #   1###2###3###4
+print('以下是参数end="\n\n\n\n\n"来换五新行')
+print(1,2,3,4,end="\n\n\n\n\n")
+print(6789,end="")
+print("我是最后一行")

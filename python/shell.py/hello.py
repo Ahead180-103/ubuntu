ae9bfab6bb862d5be155b3981c502f38dd780e35
gaoyong <ahead_888@163.com>
2018-10-13 13:06:47 +0800

    add

---
+++ python/shell.py/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+name:gaoyong
+date:2018-10-13
+content:测试演示
+'''
+
+print("hello world!")
+print("  *")
+print(" **")
+print("***")
+
+for i in range(5):
+   print("i =",i)

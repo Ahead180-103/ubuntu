618002792c01be894e6ecbd129fc3857b52372f4
gyahead <ahead_888@163.com>
2018-12-13 16:35:41 +0800

    add

--- python/shell.py/sys1.py
+++ python/shell.py/sys1.py
@@ -4,9 +4,13 @@
 import sys
 print("计算1/x的值")
 while True:
+  try:
     number = int(input("enter a number:"))
     if number == 0:
         sys.stderr.write("除以0 error\n")
     else:
-        a = "1/%d = %f" % (number,1/number)
+        a = "1/%d = %s" % (number,1/number)
         sys.stdout.write(a+"\n")
+  except ValueError:
+      print("请输入正整数")
+      exit()
